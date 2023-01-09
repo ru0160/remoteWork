7ab297f1c930556b05d3dde78c3b3e293e417868
Оришеч Александр <aleksandr.orishech@gazprombank.ru>
2023-01-09 17:04:25 +0300

    del controller, add EventListener

--- src/main/java/alex/remote/work/service/EngineServiceImpl.java
+++ src/main/java/alex/remote/work/service/EngineServiceImpl.java
@@ -3,6 +3,8 @@ package alex.remote.work.service;
 import alex.remote.work.propery.WorkProperty;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -17,6 +19,7 @@ public class EngineServiceImpl implements EngineService {
     private final WorkProperty property;
 
     @Override
+    @EventListener(ApplicationReadyEvent.class)
     public void process() throws IOException {
         clearAllSaveFile();
         copyService.copyFolder(property.getPathGradle(), property.getPathGradleCopy());
